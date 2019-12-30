6db797e615fea8eceb27a638503a721a7ccd08a9
aqcd <aquajet173@gmail.com>
2019-12-30 17:00:12 +0800

    Made imports more readable

--- app/src/packs/Index.tsx
+++ app/src/packs/Index.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import { render } from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "../components/App";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
