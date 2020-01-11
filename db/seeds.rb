6d2f86daaded732f9f774661dbeb244684cf9510
aqcd <aquajet173@gmail.com>
2020-01-11 10:44:01 +0800

    Revamp db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-3.times do |i|
-  Todo.create(
-    name: "Todo #{i + 1}",
-    by: DateTime.new(2020, 12, 31),
-    tag_list: 'Example',
-    details: 'Sample Todo',
+Todo.create(
+    name: "Celebrate Christmas",
+    by: DateTime.new(2019, 12, 25),
+    tag_list: 'Holiday',
+    details: 'Sample Todo - Todo will be coloured red when overdue.',
     completed: false
-  )
-end
+)
+Todo.create(
+    name: "Celebrate New Year",
+    by: DateTime.new(2020, 01, 01),
+    tag_list: 'Holiday',
+    details: 'Sample Todo - Todo will be coloured green when toggled complete.',
+    completed: true
+)
+Todo.create(
+    name: "Celebrate Chinese New Year",
+    by: DateTime.new(2020, 01, 25),
+    tag_list: 'Holiday',
+    details: 'Sample Todo - Select Holiday from tag search dropdown to filter by the Holiday tag.',
+    completed: false
+)
+Todo.create(
+    name: "Celebrate Labour Day",
+    by: DateTime.new(2020, 05, 01),
+    tag_list: 'Holiday',
+    details: 'Sample Todo - Todo will be uncoloured if neither overdue nor complete.',
+    completed: false
+)
+Todo.create(
+    name: "CVWO Mid Submission",
+    by: DateTime.new(2019, 12, 30),
+    tag_list: 'CVWO',
+    details: 'Sample Todo - You will not see this todo if you filter by Holiday or filter by active CVWO tags.',
+    completed: true
+)
+Todo.create(
+    name: "CVWO Final Submission",
+    by: DateTime.new(2020, 01, 25),
+    tag_list: 'CVWO',
+    details: 'Sample Todo - This will be the only sample todo you see if filtered by CVWO and active.',
+    completed: false
+)
